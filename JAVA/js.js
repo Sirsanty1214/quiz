355ff845bda00a4033bd3f677bc226edb29b16ea
Sala G7 <CSalaG7@uniboyaca.edu.co>
2024-09-19 19:52:04 -0500

    Cambio de nombres de los id correspondientes

--- JAVA/js.js
+++ JAVA/js.js
@@ -1,21 +1,20 @@
 function guardarRegistro() {
     const codigo = document.getElementById('codigo').value;
     const nombre = document.getElementById('nombre').value;
-    const porcentaje1 = document.getElementById('porcentaje1').value;
-    const porcentaje2 = document.getElementById('porcentaje2').value;
-    const porcentaje3 = document.getElementById('porcentaje3').value;
-    const porcentaje4 = document.getElementById('porcentaje4').value;
+    const nota1 = document.getElementById('nota1').value;
+    const nota2 = document.getElementById('nota2').value;
+    const nota3 = document.getElementById('nota3').value;
+    const nota4 = document.getElementById('nota4').value;
 
     const table = document.getElementById('estudiantes').getElementsByTagName('tbody')[0];
     const newRow = table.insertRow();
 
     newRow.insertCell(0).textContent = codigo;
     newRow.insertCell(1).textContent = nombre;
-    newRow.insertCell(2).textContent = porcentaje1;
-    newRow.insertCell(3).textContent = porcentaje2;
-    newRow.insertCell(4).textContent = porcentaje3;
-    newRow.insertCell(5).textContent = porcentaje4;
-
-    // Limpiar los campos del formulario
+    newRow.insertCell(2).textContent = nota1;
+    newRow.insertCell(3).textContent = nota2;
+    newRow.insertCell(4).textContent = nota3;
+    newRow.insertCell(5).textContent = nota4;
+    
     document.getElementById('registro').reset();
 }
