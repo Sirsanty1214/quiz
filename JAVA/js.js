4775f2b0047e709fee146d083d604fb903f99a25
Sirsanty1214 <sbonilla@uniboyaca.edu.co>
2024-09-20 18:59:48 -0500

    Creacion para el metodo de verfificar si el codigo ya existe y sersioramiento de que los id esten con los nombres correspoindientes, ademas creacion del boton de eliminar para las filasç

--- JAVA/js.js
+++ JAVA/js.js
@@ -1,5 +1,34 @@
+let filaSeleccionada = null; 
+
+function codigoRepetido(codigo) {
+    const table = document.getElementById('estudiantes').getElementsByTagName('tbody')[0];
+    for (let i = 0; i < table.rows.length; i++) {
+        const fila = table.rows[i];
+        if (fila.cells[1].textContent === codigo) { 
+            mostrarError(); 
+            return true; 
+        }
+    }
+    return false; 
+}
+
+function mostrarError() {
+    const modalError = document.getElementById('msg'); 
+    modalError.style.display = 'flex'; 
+}
+
+document.getElementById('cerrarError').addEventListener('click', function() {
+    const modalError = document.getElementById('msg'); 
+    modalError.style.display = 'none'; 
+});
+
 function guardarRegistro() {
     const codigo = document.getElementById('codigo').value;
+
+    if (codigoRepetido(codigo)) {
+        return; 
+    }
+
     const nombre = document.getElementById('nombre').value;
     const nota1 = document.getElementById('nota1').value;
     const nota2 = document.getElementById('nota2').value;
@@ -7,14 +36,22 @@ function guardarRegistro() {
     const nota4 = document.getElementById('nota4').value;
 
     const table = document.getElementById('estudiantes').getElementsByTagName('tbody')[0];
-    const newRow = table.insertRow();
+    const nuevaFila = table.insertRow();
 
-    newRow.insertCell(0).textContent = codigo;
-    newRow.insertCell(1).textContent = nombre;
-    newRow.insertCell(2).textContent = nota1;
-    newRow.insertCell(3).textContent = nota2;
-    newRow.insertCell(4).textContent = nota3;
-    newRow.insertCell(5).textContent = nota4;
-    
+    const celdaEliminar = nuevaFila.insertCell(0);
+    const botonEliminar = document.createElement('button');
+    botonEliminar.textContent = 'Eliminar';
+    botonEliminar.onclick = function() { mostrarModal(nuevaFila); };
+    celdaEliminar.appendChild(botonEliminar);
+
+    nuevaFila.insertCell(1).textContent = codigo;
+    nuevaFila.insertCell(2).textContent = nombre;
+    nuevaFila.insertCell(3).textContent = nota1;
+    nuevaFila.insertCell(4).textContent = nota2;
+    nuevaFila.insertCell(5).textContent = nota3;
+    nuevaFila.insertCell(6).textContent = nota4;
+
     document.getElementById('registro').reset();
 }
+
+
